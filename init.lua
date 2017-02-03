437457e3f3277cb80c7e90a3d250af4e06361c74
TumeniNodes <tumeninodes@gmail.com>
2017-02-03 12:29:14 -0500

    Update init.lua
    changed sounds for estate door from stone to wood

--- init.lua
+++ init.lua
@@ -1070,5 +1070,5 @@ doors.register("artdeco:estatedoor", {
 	tiles = {{ name = "artdeco_estate_door.png", backface_culling = true }},
 	groups = {choppy=2,cracky=2,door=1},
  	protected = false,
-sounds = default.node_sound_stone_defaults(),
+sounds = default.node_sound_wood_defaults(),
 })
